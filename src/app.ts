3c7c649c723307c6b7bcc69e06e74bec2f3a8625
idir CHAOUCHI <idirchaouchi@outlook.com>
2022-09-16 15:21:40 +0100

    ...

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.get('/', (req, res) => {
+    console.log(os.hostname())
     res.send(`Hello World! from hostname -> ${os.hostname()} ${process.pid}`);
 });
 
